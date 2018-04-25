3aa42b364922a20c58e661f86814dc1e6c64fbf5
Hazar Kaysi <enghazar@gmail.com>
2018-04-25 13:10:23 +0200

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -9,7 +9,7 @@
     <body>
         <a  href="index.php"><img src="img/banniere.jpg" height="250px" ></a>
         <p>Veuillez entrer votre login et votre mot de passe</p>
-        <form action="login.php" method="post" name="connexion">
+        <form action="admin/login.php" method="post" name="connexion">
             <p>
             <label for "login" >Login</label><br>
             <input type="text" name="login" autofocus required ><br>
