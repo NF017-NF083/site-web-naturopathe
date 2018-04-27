f4af04083ae0a867f2d62e5256c080c5086cf29b
Hazar Kaysi <enghazar@gmail.com>
2018-04-27 17:30:33 +0200

    Add files via upload

--- sophrologie.php
+++ sophrologie.php
@@ -10,18 +10,18 @@
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <link rel="icon" href="img/favicon.ico" type="image/x-icon">
     <title>L'équilibre au naturel - La Sophrologie</title>
+        <!-- Code google analytics a mettre dans chaque page qu'on souhaite suivre-->
+       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-117869108-1"></script>
+       <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+       gtag('config', 'UA-117869108-1');
+       </script>
 </head>
 
 <body>
-    <!-- Code google analytics a mettre dans chaque page qu'on souhaite suivre-->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117869108-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-117869108-1');
-</script>
 
     <a  href="index.php"><img src="img/banniere.jpg" height="250px" ></a>
     <?php include "menu.php"; ?>
